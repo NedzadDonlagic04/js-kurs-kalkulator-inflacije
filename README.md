dccad4c162437d2863b2ea55d13839fee61b5c31
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-07-04 22:08:49 +0200

    Added my project

---
+++ README.md
@@ -0,0 +1,6 @@
+"# js-kurs-kalkulator-inflacije" 
+
+Practice problem given by the following
+youtube video
+https://youtu.be/WcyFd3kPz9k?t=10018
+I only edited the script.js file
---
+++ script.js
@@ -0,0 +1,27 @@
+function inflationCalculator()
+{
+    let inflationRate=document.getElementById('inflationRate').value;
+    let money=document.getElementById('money').value;
+    let years=document.getElementById('years').value;
+
+    inflationRate=parseFloat(inflationRate);
+    money=parseFloat(money);
+    years=parseFloat(years);
+
+    console.log(inflationRate,typeof(inflationRate));
+    console.log(money,typeof(money));
+    console.log(years,typeof(years));
+
+    let worth=money+(money*(inflationRate/100));
+
+    for(let i=1;i<years;i++)
+    {
+        worth+=worth*(inflationRate/100);
+    }
+
+    let resultDiv=document.createElement('div');
+    resultDiv.className='new-value';
+    resultDiv.innerText=`Danasnjih ${money}€ vrijedi isto kao ${worth.toFixed(2)}€ za ${years} godina.`;
+
+    document.querySelector('.container').appendChild(resultDiv);
+}
